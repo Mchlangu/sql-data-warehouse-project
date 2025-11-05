43b716112fa4b59c61f02e7a35c6d1b93eed0b47
Mchlangu <57295033+Mchlangu@users.noreply.github.com>
2025-11-05 11:12:23 +0200

    Corrected script to load data in table crm_prd_info and added script description
    Corrected spelling of 'Table' in print statements and removed redundant truncation of 'bronze.crm_prd_info'.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,3 +1,22 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
 
 CREATE OR ALTER PROCEDURE bronze.full_load AS
 
@@ -33,7 +52,7 @@ BEGIN
 	
 
 		SET @start_time = GETDATE();
-		PRINT '>> Truncating Tsble: bronze.crm_prd_info';
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
 		PRINT '>> Inserting into Table: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
@@ -51,10 +70,8 @@ BEGIN
 
 		SET @start_time = GETDATE();
 
-		PRINT '>> Truncating Tsble: bronze.crm_prd_info';
-		TRUNCATE TABLE bronze.crm_prd_info;
-		SET @end_time = GETDATE();
-		PRINT 'Load time >>'  +  CAST( DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
+		PRINT '>> Truncating Tsble: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting into Table: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
 		FROM 'C:\Users\Willy\DataEngineer\DWH\datasets\source_crm\sales_details.csv'
@@ -114,7 +131,7 @@ BEGIN
 
 		SET @start_time = GETDATE();
 
-		PRINT '>> Truncating Tsble: bronze.erp_px_cat_g1v2';
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2
 		PRINT '>> Inserting into Table: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
