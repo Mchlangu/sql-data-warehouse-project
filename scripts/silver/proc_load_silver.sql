24cd10d13dc2b0183679373917ee95057faec70d
Mchlangu <57295033+Mchlangu@users.noreply.github.com>
2025-11-12 09:19:49 +0200

    Fixed: quantity and sales values swapped in the select statement

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -134,12 +134,12 @@ CREATE OR ALTER PROCEDURE silver.silver_load AS
 				ELSE CAST(CAST(sls_due_dt AS VARCHAR)AS DATE)
 			END sls_due_dt,
 
-			sls_quantity,
-
 			CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR  sls_sales != sls_quantity * ABS(sls_price)
 					 THEN sls_quantity * ABS(sls_price)
 				ELSE sls_sales
 			END sls_sales,
+
+			sls_quantity,
 
 			CASE WHEN sls_price IS NULL OR sls_price <= 0 
 					THEN sls_sales/NULLIF(sls_quantity, 0)
@@ -246,4 +246,5 @@ CREATE OR ALTER PROCEDURE silver.silver_load AS
 			PRINT 'Error Message' + CAST(ERROR_STATE() AS NVARCHAR);
 			PRINT '=================================================';
 		END CATCH
+
 	END
